c446296f3935008f6573690681c6ae6110ec16c8
ngthahndat <thanhdatdeptraidz1@gmail.com>
2025-05-25 17:48:54 +0700

    wallet.cpp

--- wallet.cpp
+++ wallet.cpp
@@ -26,8 +26,7 @@ class Wallet {
         string getID() const {
             return walletID;
         }
-    
-        void addTransaction(const Transaction& tx) {
+         void addTransaction(const Transaction& tx) {
             history.push_back(tx);
         }
     
@@ -40,28 +39,3 @@ class Wallet {
         }
     };
     
-    class Account {
-    private:
-        string username;
-        Wallet wallet;
-    
-    public:
-        Account(const string& uname, const string& walletID)
-            : username(uname), wallet(walletID) {
-            cout << " Tài khoản \"" << username << "\" đã được tạo.\n";
-        }
-    
-        string getUsername() const {
-            return username;
-        }
-    
-        Wallet& getWallet() {
-            return wallet;
-        }
-    };
-    
-    string generateOTP() {
-        srand((unsigned)time(0)); 
-        int otp = rand() % 900000 + 100000; 
-        return to_string(otp);
-    }
