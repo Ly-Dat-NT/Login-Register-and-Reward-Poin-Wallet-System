4504afeb11bf74239a852bf0d4de6b457465e2dc
Ly-Dat-NT <datly.250445@gmail.com>
2025-05-26 17:15:50 +0700

    Delete transaction.cpp

--- transaction.cpp
+++
@@ -1,9 +0,0 @@
-class Transaction {
-    public:
-        string from;
-        string to;
-        double amount;
-        string status;
-        Transaction(const string& f, const string& t, double a, const string& s)
-            : from(f), to(t), amount(a), status(s) {}
-    };
